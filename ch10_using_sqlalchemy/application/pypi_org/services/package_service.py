d28187b6969d674c39929831653d4a6b13c5eac7
vromaniuk <vromaniuk@deloitte.com>
2020-02-11 16:37:50 +0200

    replace fake function get_latest_packages to get_latest_releases

--- ch10_using_sqlalchemy/application/pypi_org/services/package_service.py
+++ ch10_using_sqlalchemy/application/pypi_org/services/package_service.py
@@ -1,14 +1,21 @@
+import sqlalchemy.orm
+from typing import List
+
 from application.pypi_org.data import db_session
 from application.pypi_org.data.package import Package
 from application.pypi_org.data.releases import Release
 
 
-def get_latest_packages():
-    return [
-        {'name': 'flask', 'version': '1.2.3'},
-        {'name': 'sqlalchemy', 'version': '2.2.0'},
-        {'name': 'passlib', 'version': '3.0.0'},
-    ]
+def get_latest_releases(limit=10) -> List[Release]:
+    session = db_session.create_session()
+    releases = session.query(Release). \
+        options(sqlalchemy.orm.joinedload(Release.package)). \
+        order_by(Release.created_date.desc()). \
+        limit(limit). \
+        all()
+
+    session.close()
+    return releases
 
 
 def get_package_count() -> int:
--- ch10_using_sqlalchemy/application/pypi_org/views/home_views.py
+++ ch10_using_sqlalchemy/application/pypi_org/views/home_views.py
@@ -9,9 +9,8 @@ blueprint = Blueprint('home', __name__, template_folder='templates')
 @blueprint.route('/')
 @response(template_file='home/index.html')
 def index():
-    test_packages = package_service.get_latest_packages()
     return {
-        'packages': test_packages,
+        'releases': package_service.get_latest_releases(),
         'package_count': package_service.get_package_count(),
         'release_count': package_service.get_release_count(),
         'user_count': user_service.get_user_count(),
