adc0138dbd4b9aeea2bef6ca3dc68218c78023c1
Romaniuk Vlad <romanuk_vlad@ukr.net>
2020-03-22 22:00:45 +0200

    update sqlalchemy queries to mongodb's

--- ch16_mongodb/application/pypi_org/services/package_service.py
+++ ch16_mongodb/application/pypi_org/services/package_service.py
@@ -1,34 +1,23 @@
-import sqlalchemy.orm
 from typing import List, Optional
 
-from sqlalchemy.orm import Session
-
-from application.pypi_org.data import db_session
-from application.pypi_org.data.package import Package
-from application.pypi_org.data.releases import Release
+from application.pypi_org.nosql.packages import Package
+from application.pypi_org.nosql.releases import Release
 
 
 def get_latest_releases(limit=10) -> List[Release]:
-    session = db_session.create_session()
-
-    releases = session.query(Release). \
-        options(sqlalchemy.orm.joinedload(Release.package)). \
-        order_by(Release.created_date.desc()). \
+    releases = Release.objects(). \
+        order_by("-created_date"). \
         limit(limit). \
         all()
-    session.close()
-
     return releases
 
 
 def get_package_count() -> int:
-    session = db_session.create_session()
-    return session.query(Package).count()
+    return Package.objects().count()
 
 
 def get_release_count() -> int:
-    session = db_session.create_session()
-    return session.query(Release).count()
+    return Release.objects().count()
 
 
 def get_package_by_id(package_id: str) -> Optional[Package]:
@@ -37,24 +26,13 @@ def get_package_by_id(package_id: str) -> Optional[Package]:
 
     package_id = package_id.strip().lower()
 
-    session = db_session.create_session()
-
-    package = session.query(Package) \
-        .options(sqlalchemy.orm.joinedload(Package.releases)) \
-        .filter(Package.id == package_id) \
-        .first()
-
-    session.close()
+    package = Package.objects().filter(id=package_id).first()
 
     return package
 
 
 def all_packages(limit: int) -> List[Package]:
-    session: Session = db_session.create_session()
-    try:
-        return list(session.query(Package).limit(limit))
-    finally:
-        session.close()
+    return list(Package.objects().limit(limit))
 #
 # def test():
 #     """
--- ch16_mongodb/application/pypi_org/services/user_service.py
+++ ch16_mongodb/application/pypi_org/services/user_service.py
@@ -1,17 +1,14 @@
 from typing import Optional
 from passlib.handlers.sha2_crypt import sha512_crypt as crypto
-from application.pypi_org.data import db_session
-from application.pypi_org.data.users import User
+from application.pypi_org.nosql.users import User
 
 
 def get_user_count() -> int:
-    session = db_session.create_session()
-    return session.query(User).count()
+    return User.objects().count()
 
 
 def find_user_by_email(email: str) -> Optional[User]:
-    session = db_session.create_session()
-    return session.query(User).filter(User.email == email).first()
+    return User.objects().filter(email=email).first()
 
 
 def create_user(name: str, email: str, password: str) -> Optional[User]:
@@ -23,9 +20,7 @@ def create_user(name: str, email: str, password: str) -> Optional[User]:
     user.name = name
     user.hashed_password = hash_text(password)
 
-    session = db_session.create_session()
-    session.add(user)
-    session.commit()
+    user.save()
 
     return user
 
@@ -40,8 +35,7 @@ def verify_hash(hashed_text: str, plain_text: str) -> bool:
 
 
 def login_user(email: str, password: str) -> Optional[User]:
-    session = db_session.create_session()
-    user = session.query(User).filter(User.email == email).first()
+    user = find_user_by_email(email)
     if not user:
         return None
     if not verify_hash(user.hashed_password, password):
@@ -50,6 +44,5 @@ def login_user(email: str, password: str) -> Optional[User]:
 
 
 def find_user_by_id(user_id: int) -> Optional[User]:
-    session = db_session.create_session()
-    user = session.query(User).get(user_id)
+    user = User.objects().filter(id=user_id).first()
     return user
